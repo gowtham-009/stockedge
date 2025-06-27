5a4ad70d3f7ba74c4eee768eb2ceaf5815a4cbfd
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-06-27 15:22:26 +0530

    Update tips.js

--- script/tips.js
+++ script/tips.js
@@ -346,7 +346,7 @@ function renderGrid(data) {
   gridContainer.innerHTML = `
     <ul role="list" class="grid-content mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
       ${data.map(item => `
-        <li class="col-span-1 px-2 py-2 flex rounded-md shadow-lg">
+        <li class="col-span-1 px-2 py-2 flex rounded-md border-2">
           <div class="w-full">
             <div class="w-full flex gap-1 items-center">
               <div>
